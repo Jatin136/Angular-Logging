74f77af2480812034bfcf735172c74068b829298
Jatin Patel <jatinpatel136@gmail.com>
2017-12-17 21:09:05 +0530

    resolution for Cannot read property 'config' of null

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { LoggingService } from './logging.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
 
